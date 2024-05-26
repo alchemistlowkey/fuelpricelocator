ccefc06ed531002f6aed32e88f4b0be29170c670
alchemistlowkey <alchemistlowkey@gmail.com>
2024-05-26 09:35:51 +0100

    User model created

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const productEnum = ["PMS", "AGO", "LPG"];
+
+const userSchema = new Schema(
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      product: {
+        type: String,
+        enum: productEnum,
+        required: true
+      },
+      stationName: {
+        type: String,
+        required: [true, "Please enter the fueling station name"]
+      }
+    },
+    {
+      timestamps: true,
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
