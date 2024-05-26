981ac2905b370eaf8398a0d53167555102a36323
alchemistlowkey <alchemistlowkey@gmail.com>
2024-05-26 08:36:13 +0100

    environment setup

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<main>
+	<p>Fuel Price Locator</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
