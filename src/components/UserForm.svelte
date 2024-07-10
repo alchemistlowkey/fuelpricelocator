862db60507d6a7289e9af616d43a0509aeb183d7
alchemistlowkey <alchemistlowkey@gmail.com>
2024-07-10 23:25:23 +0100

    frontend: backend url updated 1

--- src/components/UserForm.svelte
+++ src/components/UserForm.svelte
@@ -19,7 +19,7 @@
   const handleSubmit = async (event) => {
     try {
       // Send a POST request to the server to create a new user
-      const response = await fetch("http://localhost:5000/users", {
+      const response = await fetch("https://backend-gamma-ashen.vercel.app/users", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
